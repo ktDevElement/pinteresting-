58a85788efa7b9b4434c1198ffd8ceca998ab4e1
Kt McCormick <ktmccormick13@gmail.com>
2019-05-10 16:47:59 -0500

    change the devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
- validates :password, :presence => true,
-                     :on => :create,
-                     :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+  # attr_accessor :email, :password, :password_confirmation
+  # validates :password, :presence => true,
+  # 					   :confirmation => true, 
+  # 					   :length => {:within => 6..40},
+  #                      :on => :create, 
+  #                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+  
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable 
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
